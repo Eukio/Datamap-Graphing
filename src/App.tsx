0f729e9d6d772563ae1864568f6b926968a16c10
Eucharist Tan <Euki16321@gmail.com>
2025-07-14 17:22:55 -0500

    recommit

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,120 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+import Graph from './components/Graph.tsx';
+import './App.css';
+import * as d3 from 'd3';
+
+
+const filename: string = 'test.csv';
+
+
+interface Data {
+  name: string;
+  type?: 'line';
+  stack?: 'Total';
+  values: Array<[timestamp: string, value: number]>;
+}
+
+
+const searchedValues: Set<string> = new Set([
+  'ctOutdoorCoolRequestedDemand',
+  'ctAHHeatRequestedDemand',
+  'ctOutdoorHeatRequestedDemand',
+  'ctIFCHeatRequestedDemandPercent',
+  'ctIFCCoolRequestedDemandPercent',
+  'ctOutdoorDeHumidificationRequestedDemand',
+  'ctIFCHumRequestedDemandPercent'
+]);
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [chartOption, setChartOption] = useState({});
+  const [dataSets, setDataSets] = useState<Data[]>([]);
+
+
+  useEffect(() => {
+    d3.csv(filename).then((loadedData) => {
+      const columns = loadedData.columns.filter((col) =>
+        searchedValues.has(col)
+      );
+
+
+      const parsedData: Data[] = columns.map((name) => {
+        const values: Array<[string, number]> = [];
+
+
+        loadedData.forEach((row) => {
+          const timeStamp = row['record_time'];
+          const yValue = row[name];
+
+
+          if (timeStamp && yValue) {
+            values.push([timeStamp, parseFloat(yValue)]);
+          }
+        });
+
+
+        return {
+          name,
+          type: 'line',
+          stack: 'Total',
+          values
+        };
+      });
+      setDataSets(parsedData);
+    });
+  }, []);
+
+
+  useEffect(() => {
+    if (dataSets.length === 0) return;
+
+
+    setChartOption({
+      animation: false,
+      tooltip: { trigger: 'axis' },
+      legend: { data: dataSets.map((d) => d.name) },
+      grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true
+      },
+      toolbox: {
+        feature: {
+          dataZoom: { yAxisIndex: 'none' },
+          saveAsImage: {}
+        }
+      },
+      xAxis: { type: 'time' },
+      yAxis: { type: 'value', boundaryGap: [0, '100%'] },
+      dataZoom: [
+        {
+          type: 'slider',
+          show: true,
+          realtime: false
+        }
+      ],
+      series: dataSets.map((obj) => ({
+        name: obj.name,
+        type: 'line',
+        large: true,
+        largeThreshold: 10000,
+        data: obj.values,
+        symbol: 'none'
+      }))
+    });
+  }, [dataSets]);
+
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Graph option={chartOption} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+
+export default App;
+
---
+++ src/components/Graph.tsx
@@ -0,0 +1,73 @@
+import React, { useRef, useEffect } from "react";
+import { init, getInstanceByDom } from "echarts";
+import type { CSSProperties, JSX } from "react";
+import type { EChartsOption, ECharts, SetOptionOpts } from "echarts";
+
+
+export interface ReactEChartsProps {
+  option: EChartsOption;
+  style?: CSSProperties;
+  settings?: SetOptionOpts;
+  loading?: boolean;
+  theme?: "light" | "dark";
+}
+
+
+export function Graph({
+  option,
+  style,
+  settings,
+  loading,
+  theme,
+}: ReactEChartsProps): JSX.Element {
+  const chartRef = useRef<HTMLDivElement>(null);
+
+
+  useEffect(() => {
+    let chart: ECharts | undefined;
+    if (chartRef.current !== null) {
+      chart = init(chartRef.current, theme);
+    }
+
+
+    function resizeChart() {
+      chart?.resize();
+    }
+
+
+    window.addEventListener("resize", resizeChart);
+    return () => {
+      chart?.dispose();
+      window.removeEventListener("resize", resizeChart);
+    };
+  }, [theme]);
+
+
+  useEffect(() => {
+    if (chartRef.current !== null ) {
+      const chart = getInstanceByDom(chartRef.current);
+      chart?.setOption(option, settings);
+    }
+  }, [option, settings, theme]);  
+
+
+  useEffect(() => {
+    if (chartRef.current !== null) {
+      const chart = getInstanceByDom(chartRef.current);
+      if (loading) {
+        chart?.showLoading();
+      } else {
+        chart?.hideLoading();
+      }
+    }
+  }, [loading, theme]);
+
+
+  return (
+    <div ref={chartRef} style={{ width: "55rem", height: "35rem", ...style }} />
+  );
+}
+
+
+export default Graph;
+
